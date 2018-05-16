31e85bf9001f19ad49d55a45e38480fd5efbd75d
Jace Mitsuka <PJace@BigMac.local>
2018-05-15 16:34:51 -1000

    imported forms, httpclient, and router modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
