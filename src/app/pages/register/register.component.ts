ac0768b834dbe34068be77f0c76e02ac2b4c2da3
Jace Mitsuka <PJace@BigMac.local>
2018-05-15 16:35:53 -1000

    created registration component and service

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RegisterService } from '../../services/register.service';
+
+@Component({
+    templateUrl: './register.component.html',
+    styleUrls: ['./contact.component.scss']
+})
+export class RegisterComponent {
+    registerData: Object = {
+        username: '',
+        email: '',
+        password: '',
+        first_name: '',
+        last_name: '',
+        location: '',
+        age: 0
+    };
+    constructor (private registerService: RegisterService) {
+
+    }
+
+    submit(e) {
+        e.preventDefault();
+        console.log(e);
+        this.registerService.submitRegistration(this.registerData)
+        .toPromise()
+        .then((data) => { console.log(data); })
+        .catch((err) => { console.log(err); });
+    }
+}
---
+++ src/app/services/register.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RegisterService {
+    constructor(private http: HttpClient) { }
+
+    submitRegistration(data) {
+        return this.http.post('http://somurl.com', data);
+    }
+}
